effe94082a10c5c7cbf9f5e7b3c87e8f9fad19e1
devbridgelabs <devbridgelabs@gmail.com>
2022-10-05 11:59:44 -0300

    first commit

---
+++ android/app/src/main/kotlin/br/com/flutterbridgelabs/qr_code/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.flutterbridgelabs.qr_code
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _scanBarcode = 'vazio';
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<void> scanQR() async {
+    String barcodeScanRes;
+    try {
+      barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+          '#ff6666', 'Cancelar', true, ScanMode.QR);
+      print(barcodeScanRes);
+    } on PlatformException {
+      barcodeScanRes = 'Failed to get platform version.';
+    }
+    setState(() {
+      _scanBarcode = barcodeScanRes;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(title: const Text('QR code')),
+            body: Builder(builder: (BuildContext context) {
+              return Container(
+                  alignment: Alignment.center,
+                  child: Flex(
+                      direction: Axis.vertical,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        ElevatedButton(
+                            onPressed: () => scanQR(),
+                            child: const Text('QR scan')),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 60),
+                          child: Text('Resultado : $_scanBarcode\n',
+                              style: const TextStyle(fontSize: 23)),
+                        )
+                      ]));
+            })));
+  }
+}
